374f9de5831d3bf6afd8cc26427021395f97004c
Rasel Khan <raselk.ptl21@gmail.com>
2023-03-20 16:26:33 +0600

    update image upload

--- lib/app/data/callbacks/callbacks.dart
+++ lib/app/data/callbacks/callbacks.dart
@@ -1,3 +1,5 @@
+import 'package:dio/dio.dart';
+
 import '../services/api_service.dart';
 
 abstract class CallBack {
@@ -12,4 +14,10 @@ abstract class CallBack {
       {required String endPoint,
       required Method method,
       required Map<String, dynamic> map});
+
+
+  Future<dynamic> uploadBaseApi(
+      {required String endPoint,
+      required Method method,
+      required FormData map});
 }
